c64795901864bf42209110c9bc08420166293caf
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-06-25 12:14:17 +0300

    App with input

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
+conn, addr = sock.accept()
+
+print 'connected:', addr
+
 while True:
-    conn, addr = sock.accept()
     data = conn.recv(1024)
     if not data:
         break
     conn.send(data.upper())
-    conn.close()
+
+conn.close()
 #Test comment
