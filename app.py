4714c3cc90cc1606567624d388f41dbff58957f0
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-06-25 17:59:53 +0300

    app

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
+
 #!/usr/bin/env python
 import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
-conn, addr = sock.accept()
-
-print 'connected:', addr
-
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
-
-conn.close()
+    conn, addr = sock.accept()
+    conn.send('Hello from Server!'.encode())
+    conn.close()
 #Test comment
+
